0ad1c0a076ed58fa0621058bae4e361463fecd83
clovisdasilvaneto <cloves13@hotmail.com>
2015-08-05 00:17:02 -0300

    first commit to version 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
-	minifycss = require('gulp-minify-css'),
 	notify = require('gulp-notify'),
-	concat = require('gulp-concat'),
 	src = 'assets';
 
 gulp.task('css', function() {
@@ -12,14 +10,6 @@ gulp.task('css', function() {
 		.pipe(notify('Stylus compilado e mimificado!'));
 });
 
-gulp.task('dist-css', function() {
-	gulp.src('./'+src+'/css/all-animation.css')
-		.pipe(concat('./all-animation.min.css'))
-		.pipe(minifycss())
-		.pipe(gulp.dest('./'+src+'/css'))
-		.pipe(notify('All animaiton is begin created!'));
-});
-
 gulp.task('default', function() {
-	gulp.watch('./'+src+'/stylus/**/**/*.*', ['css', 'dist-css']);
+	gulp.watch('./'+src+'/stylus/**/**/*.*', ['css']);
 });
