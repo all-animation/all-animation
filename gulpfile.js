66a0585dd197e529764cdb27587b6ee700c0c14d
Clovis Neto <clovis.neto@liferay.com>
2017-12-14 13:06:36 -0300

    Adjust gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
 	notify = require('gulp-notify'),
-	src = 'assets';
+	dist = 'dist',
+	src = 'src';
 
 gulp.task('css', function() {
 	gulp.src('./'+src+'/stylus/all-animation.styl')
 		.pipe(stylus())
-		.pipe(gulp.dest('./'+src+'/css'))
+		.pipe(gulp.dest('./'+dist+'/css'))
 		.pipe(notify('Stylus compilado e mimificado!'));
 });
 
 // Settings tasks
 gulp.task('watch', function() {
-  gulp.watch('./'+src+'/stylus/**/**/*.*', ['css']);
+  gulp.watch('./'+src+'/stylus/**/*.*', ['css']);
 });
 
 gulp.task('default', ['css', 'watch']);
